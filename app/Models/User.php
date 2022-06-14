5b1d8057e567f1f858ce0c2bec289b80f37497fb
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-06-14 16:13:51 +0700

    add hasmany relation to message

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function customer() {
+    public function customer()
+    {
         return $this->belongsTo(Customer::class);
+    }
+    public function message()
+    {
+        return $this->hasMany(Message::class);
     }
 }
