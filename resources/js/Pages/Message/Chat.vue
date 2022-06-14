d40e34d690c80379ef73c56751117e6f0b6f98e9
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-06-14 16:17:28 +0700

    component chat for showing message

---
+++ resources/js/Pages/Message/Chat.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div v-for="(message, index) in messages" :key="index" class="chat-history">
+    <ul class="m-b-0">
+      <li class="clearfix">
+        <div class="message-data">
+          <span class="message-data-time">10:12 AM, Today</span>
+        </div>
+        <div class="message other-message float-right">
+          {{ message.message }}
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { onMounted, ref } from "vue";
+const emit = defineEmits({});
+const props = defineProps({
+  messages: Object,
+});
+
+const messages = ref(props.messages);
+
+// onMounted(() => {
+//   console.log(props.messages);
+// });
+</script>
