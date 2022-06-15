4a2c87cce89ff924cd7f8cdf3f4476a664c70b58
Ariq <jalanpari2@gmail.com>
2022-06-15 22:06:02 +0700

    style baru

---
+++ resources/views/user/style-daftar.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!-- Coding by CodingLab | www.codinglabweb.com-->
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<title> Registration or Sign Up form in HTML CSS | CodingLab </title>-->
+    <link href="{{ asset('assets/vendor/animate.css/animate.compat.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/regis.css') }} ">
+
+    @yield("title")
+</head>
+
+<body>
+    @yield("content")
+</body>
+
+
+</html>
