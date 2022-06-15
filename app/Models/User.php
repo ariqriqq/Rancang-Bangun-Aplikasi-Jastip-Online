f7f61944ea596699511d5d425d4bc3a06269cc96
Ariq <jalanpari2@gmail.com>
2022-06-16 01:02:28 +0700

    relasi database

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Message::class);
     }
+
+    public function jastiper()
+    {
+        return $this->hasMany(Jastiper::class);
+    }
 }
