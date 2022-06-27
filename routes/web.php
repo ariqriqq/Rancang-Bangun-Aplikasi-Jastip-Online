a2ce86f375131ccd94cbe5f10f13c5667f7a2836
Ariq <jalanpari2@gmail.com>
2022-06-27 16:55:49 +0700

    menambahkan routing

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::resource('/customer', 'CustomerController')->middleware('auth');
 Route::resource('/jastiper', 'JastiperController')->middleware('auth');
 
 Route::get('/order', function () {
-    return view('page.order');
+    return view('page.customer.order');
 })->middleware('auth');
 
 Route::get('/daftar', function () {
@@ -29,15 +29,15 @@ Route::get('/daftar', function () {
 });
 
 Route::get('/bejastiper', function () {
-    return view('page.bejastiper');
+    return view('page.jastiper.bejastiper');
 })->middleware('auth');
 
 Route::get('/myorder', function () {
-    return view('page.myorder');
+    return view('page.customer.myorder');
 })->middleware('auth');
 
 Route::get('/bukajasa', function(){
-    return view('page.bukajasa');
+    return view('page.jastiper.bukajasa');
 })->middleware('auth');
 
 Route::get('/login', function () {
@@ -45,9 +45,16 @@ Route::get('/login', function () {
 })->name('login');
 
 Route::get('/verifikasi', function () {
-    return view('page.verifikasi');
+    return view('page.jastiper.verifikasi');
 });
 
+Route::get('/pembayaran', function () {
+    return view('page.customer.pembayaran');
+});
+
+Route::get('/history', function () {
+    return view('page.customer.history');
+});
 // Route::get('/', function () {
 //     return view('page.home');
 // });
