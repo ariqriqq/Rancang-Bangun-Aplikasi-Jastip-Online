99cd42de0b87b511f061ba4c11e2da8dd345a4ea
Ariq <jalanpari2@gmail.com>
2022-07-07 23:04:56 +0700

    menambahkan controller

---
+++ app/Http/Controllers/JasaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\Jasa;
+use Illuminate\Support\Facades\Auth;
+
+class JasaController extends Controller
+{
+    //
+}
